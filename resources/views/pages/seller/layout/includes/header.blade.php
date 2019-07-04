27e91a278a760d9610c0588519f68ec59d669be1
markarik <kariukimarkg@gmail.com>
2019-07-04 16:33:44 +0300

    pages upload

--- resources/views/pages/seller/layout/includes/header.blade.php
+++ resources/views/pages/seller/layout/includes/header.blade.php
@@ -1,38 +1,3 @@
 
-<div class="header head_bar">
-    <div class="container">
-        <div class="row">
-            <div class="col-md-5">
-                <!-- Logo -->
-                <div class="logo">
-                    <a href="#"> <img src="{{asset('shoppy.png')}}" width="80" height="40" alt=""></a>
-                </div>
-            </div>
 
-            <div class="col-md-2 pull-right">
-                <div class="navbar navbar-inverse" role="banner">
-                  <nav class="collapse navbar-collapse bs-navbar-collapse navbar-right" role="navigation">
-                        <ul class="nav navbar-nav">
-                            <li class="dropdown">
-                                <a href="{{url('#')}}" class="dropdown-toggle" data-toggle="dropdown">MARK<b
-                                            class="caret"></b></a>
-                                <ul class="dropdown-menu animated fadeInUp">
-                                    <li><a href="{{url('/')}}">Front End</a></li>
-                                    <li><a href="{{url('/logout')}}">Logout</a></li>
-                                </ul>
-                            </li>
-                        </ul>
-                    </nav>
-
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-
-
-
-{{--{{auth :: user()->name}} --}}
-
-
-
+<h1>Side Nav</h1>
